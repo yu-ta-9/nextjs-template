8fd29961d81ef9bcdec8c5a0e04c051075a682df
Yuta Matsuo <y.matsuo.dev@gmail.com>
2025-02-24 19:43:57 +0900

    chore: update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,16 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginConfigPrettier from 'eslint-config-prettier';
+import pluginJsxA11y from 'eslint-plugin-jsx-a11y';
+import pluginReact from 'eslint-plugin-react';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+import pluginImport from 'eslint-plugin-import';
+import pluginUnusedImports from 'eslint-plugin-unused-imports';
+import typescriptEslint from 'typescript-eslint';
+import typescriptEslintParser from '@typescript-eslint/parser';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +20,59 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: ['.next', 'dist/**'],
+  },
+  pluginJs.configs.recommended,
+  ...typescriptEslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  ...compat.extends('next/core-web-vitals'),
+  pluginConfigPrettier,
+  {
+    files: ['**/*.{js,ts,tsx}'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      parser: typescriptEslintParser,
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ['eslint.config.mjs', 'postcss.config.mjs'],
+        },
+        tsconfigRootDir: import.meta.dirname,
+        project: './tsconfig.json',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      import: pluginImport,
+      'unused-imports': pluginUnusedImports,
+      'jsx-a11y': pluginJsxA11y,
+      'react-hooks': pluginReactHooks,
+    },
+    rules: {
+      ...pluginReactHooks.configs.recommended.rules,
+      '@next/next/no-img-element': 'off',
+      'import/no-anonymous-default-export': ['error', { allowArray: false }],
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'error',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'all',
+          argsIgnorePattern: '^_',
+        },
+      ],
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/consistent-type-imports': 'error',
+      '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+    },
+  },
 ];
 
 export default eslintConfig;
